c6d1447d72e637954caf17b50e504cdf74ff6260
imsaifun <imsaifun@gmail.com>
2022-06-08 12:01:00 +0600

    route create

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
 const app = express()
 
 dotenv.config()
@@ -24,6 +28,12 @@ mongoose.connection.on("connected", () => {
 app.get("/users", (req, res) => {
     res.send("Hello first request")
 })
+
+// middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 
 
