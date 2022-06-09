a22bd66b43e0ac0df6d0c3a1fb6050c836fcae60
imsaifun <imsaifun@gmail.com>
2022-06-09 17:33:04 +0600

    cookie

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -35,11 +35,7 @@ export const login = async (req, res, next) => {
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
         if (!isPasswordCorrect) return next(createError(400, "Wrong pssword or username!"));
 
-        const token = jwt.sign({
-            id: user._id,
-            isAdmin: user.isAdmin
-        },
-            process.env.JWT)
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
 
         const { password, isAdmin, ...otherDetails } = user._doc;
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,27 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next)=>{
+    res.send("Hello user, you are logged in")
+})
+router.get("/checkuser/:id", verifyToken, (req, res, next)=>{
+    res.send("Hello user, you are logged in and you can delete your account")
+})
+
+
+//Update
+router.put("/:id", updateUser)
+
+//Delete
+router.delete("/:id", deleteUser)
+
+//Get
+router.get("/:id", getUser)
+
+//Get All
+router.get("/", getUsers)
 
 
 
