f9373593e45e82a6a674070ec0df8946f886c75e
imsaifun <imsaifun@gmail.com>
2022-06-13 20:17:02 +0600

    initial

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -3,7 +3,7 @@ import "./featured.css";
 
 const Featured = () => {
   const { data, loading, error } = useFetch("/hotels/countByCity?cities=berlin,madrid,london")
-  console.log(data);
+  // console.log(data);
   return (
     <div className="featured">
       {
