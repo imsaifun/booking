bd1c48e6cb006174448300d6a626ae62fb5f0081
imsaifun <imsaifun@gmail.com>
2022-06-13 18:47:43 +0600

    initial

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,22 +1,24 @@
 import express from "express";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 //Create
-router.post("/",verifyAdmin, createHotel);
+router.post("/", verifyAdmin, createHotel);
 
 //Update
-router.put("/:id", verifyAdmin,updateHotel)
+router.put("/:id", verifyAdmin, updateHotel)
 
 //Delete
-router.delete("/:id",verifyAdmin, deleteHotel)
+router.delete("/:id", verifyAdmin, deleteHotel)
 
 //Get
-router.get("/:id", getHotel)
+router.get("/find/:id", getHotel)
 
 //Get All
 router.get("/", getHotels)
+router.get("/countByCity", countByCity)
+router.get("/countByType", getHotels)
 
 
 export default router;
